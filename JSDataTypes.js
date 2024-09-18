c1de349715e7df73fc03fe35cc5936fbf1943d4a
RaisAfkar <afkarrais27@gmail.com>
2024-09-18 12:02:06 +0700

    Materi dan tugas JS Data Types

---
+++ JSDataTypes.js
@@ -0,0 +1,36 @@
+// Dinamic Data Types
+
+// Tipe Data Number
+let x = 5;
+console.log(x);
+
+//Tipe Data String: Ditambahkan ""/'' saat input datanya
+let data1 = "Saya belajar JS"
+let data2 = "di eduwork"
+console.log(data1);
+console.log(data1+ " "+data2);
+
+// Tipe Data boolean: True or False
+let a,b,c;
+a = 10;
+b = 5;
+c = 10;
+console.log((a == b) + " " + (a==c));
+
+//Tipe data Array
+const dataMakanan = ['Sosis Bakar', 'Dimsum', 'Pisang Goreng'];
+console.log(dataMakanan[1]);
+
+// Tipe Data object
+const dataPerson = {
+    firstName:'Rais',
+    lastName:'Afkar',
+    age:'29',
+    email: 'xxxxxxxxxgmail.com'
+}
+console.log(dataPerson);
+console.log(dataPerson.firstName+ " is " +dataPerson.age+ " years old");
+
+// type of
+let length = 16;
+console.log(typeof length)
---
+++ tugasJsDataTypes.js
@@ -0,0 +1,11 @@
+let length = 16;
+console.log(typeof length);
+
+let lastName = "Johnson";
+console.log(typeof lastName);
+
+const x = {
+    firstName: "Rais",
+    lastName: "Afkar" 
+};
+console.log(typeof x);
